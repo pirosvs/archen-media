b16ad0c9d0fbc593b96acd7a5681caab8f809827
darmstrong <armstrong2579@gmail.com>
2022-08-18 17:34:20 -0700

    add reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: [
+        {
+            default: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+    ],
+    reactionBody: [
+        {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+    ],
+    createdAt: [
+        {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+        },
+    ],
+    username: [
+        {
+            type: String,
+            required: true,
+        },
+    ],
+});
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
