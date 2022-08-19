60661a22ae9a4cac05a5e8d71b7b4ab0e987f1a4
darmstrong <armstrong2579@gmail.com>
2022-08-18 18:54:25 -0700

    add ex data to move around to see if project works

---
+++ utils/data.js
@@ -0,0 +1,26 @@
+const usernames= [
+    'futuretone',
+    'megamix',
+    'projectx',
+    'projectarcade'
+];
+
+const emails = [
+    'ilovefuturetone@diva.com',
+    'ilovemegamix@diva.com',
+    'iloveprojectx@diva.com',
+    'iloveprojectarcade@diva.com'
+];
+
+const thoughts= [
+    'Wow I sure love Future Tone also I own a PlayStation console',
+    'If you stare down the last 30 seconds of The Intense Voice on extra extreme 10 times it becomes readable actually',
+    'Wow I sure love how Project DIVA games displays notes it doesnt make it hard to read at all c:',
+    'Its kind of fucked up that they display those notes in a different speed than they have to be played in',
+    'I wonder if Jerome would enjoy the song Envy Catwalk',
+    'Rhythm games are overrated booooo booooo stop playing them',
+    'Alien Alien is a nicer popular rhythm game song to adapt to new games with than Senbonzakura because it doesnt have the Japanese imperialism motifs',
+    'I have access to an arcade somehow isnt that neat!',
+    'It is neat that Project X has like a module system, wonder if therell be any more songs in this :c',
+    'I do not have a gaming computer so Mega Mix is the game for me!'
+];
---
+++ utils/seed.js
@@ -0,0 +1,4 @@
+console.table(users);
+console.table(thoughts);
+console.info('Seeding complete! 🌱');
+process.exit(0);
